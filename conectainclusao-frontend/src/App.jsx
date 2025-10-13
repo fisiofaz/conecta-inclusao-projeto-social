cba25756056b4d8d8a183d33e08337e4dfa4734c
Thaissa Padilha <thaissa_viana.padilha@hotmail.com>
2025-10-13 19:47:44 -0300

    subindo projeto

--- conectainclusao-frontend/src/App.jsx
+++ conectainclusao-frontend/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import HomePage from "./pages/HomePage";
@@ -18,29 +17,6 @@ import Footer from "./components/Footer";
 import PrivateRoute from "./components/PrivateRoute";
 import UserListPage from "./pages/Users/UserListPage";
 import UserForm from "./pages/Users/UserForm";
-=======
-import { Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/Auth/LoginPage';
-import RegisterPage from './pages/Auth/RegisterPage';
-import OpportunityListPage from './pages/Opportunities/OpportunityListPage';
-import OpportunityDetailsPage from './pages/Opportunities/OpportunityDetailsPage';
-import OpportunityForm from './pages/Opportunities/OpportunityForm';
-import ComplaintListPage from './pages/Complaints/ComplaintListPage';
-import ComplaintDetailsPage from './pages/Complaints/ComplaintDetailsPage';
-import ComplaintForm from './pages/Complaints/ComplaintForm';
-import HealthResourceListPage from './pages/HealthResources/HealthResourceListPage';
-import HealthResourceDetailsPage from './pages/HealthResources/HealthResourceDetailsPage';
-import HealthResourceForm from './pages/HealthResources/HealthResourceForm';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import PrivateRoute from './components/PrivateRoute';
-import UserListPage from './pages/Users/UserListPage';
-import UserForm from './pages/Users/UserForm';
-import SaudeBemEstar from "./pages/HealthResources/SaudeBemEstar";
-
-
->>>>>>> 812515e8b973fa7983fbbf454da2cb037247a96b
 
 function App() {
   return (
@@ -79,7 +55,6 @@ function App() {
           {/* Rotas de Denúncias */}
           <Route path="/complaints" element={<ComplaintListPage />} />
           <Route path="/complaints/:id" element={<ComplaintDetailsPage />} />
-<<<<<<< HEAD
           <Route
             path="/complaints/new"
             element={
@@ -163,50 +138,6 @@ function App() {
               </PrivateRoute>
             }
           />
-=======
-          <Route path="/complaints/new" element={
-            <PrivateRoute allowedRoles={['ROLE_USER', 'ROLE_EMPRESA', 'ROLE_ORGAO_APOIO', 'ROLE_ADMIN']}> 
-              <ComplaintForm />
-            </PrivateRoute>
-          } />
-          <Route path="/complaints/edit/:id" element={
-            <PrivateRoute allowedRoles={['ROLE_ADMIN']}>
-              <ComplaintForm />
-            </PrivateRoute>
-          } />
-          
-           {/* Rotas de Recursos de Saúde */}
-          <Route path="/health-resources" element={<HealthResourceListPage />} />
-          <Route path="/health-resources/:id" element={<HealthResourceDetailsPage />} />
-          <Route path="/health-resources/new" element={
-            <PrivateRoute allowedRoles={['ROLE_ORGAO_APOIO', 'ROLE_ADMIN']}> {/* Apenas ORGAO_APOIO ou ADMIN criam */}
-              <HealthResourceForm />
-            </PrivateRoute>
-          } />
-          <Route path="/health-resources/edit/:id" element={
-            <PrivateRoute allowedRoles={['ROLE_ORGAO_APOIO', 'ROLE_ADMIN']}> {/* Apenas ORGAO_APOIO ou ADMIN editam */}
-              <HealthResourceForm />
-            </PrivateRoute>
-          } />
-
-          {/* Página de Saúde & Bem-Estar personalizada */}
-          <Route path="/saude" element={<SaudeBemEstar />} />
-          <Route path="/users" element={
-            <PrivateRoute allowedRoles={['ROLE_ADMIN']}> {/* Apenas ADMIN pode ver a lista de usuários */}
-              <UserListPage />
-            </PrivateRoute>
-          } />
-          <Route path="/users" element={
-            <PrivateRoute allowedRoles={['ROLE_ADMIN']}> {/* Apenas ADMIN pode ver a lista de usuários */}
-              <UserListPage />
-            </PrivateRoute>
-          } />
-          <Route path="/users/edit/:id" element={
-            <PrivateRoute allowedRoles={['ROLE_ADMIN']}> {/* Apenas ADMIN pode editar usuários */}
-              <UserForm />
-            </PrivateRoute>
-          } />
->>>>>>> 812515e8b973fa7983fbbf454da2cb037247a96b
           <Route path="*" element={<h2>404 - Página Não Encontrada</h2>} />
         </Routes>
       </main>
