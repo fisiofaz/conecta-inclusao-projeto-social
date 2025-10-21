799f7eb39dd5d8401ca102132a9ae8b032c4a73f
fisiofaz <75371386+fisiofaz@users.noreply.github.com>
2025-10-20 21:50:18 -0300

    fix(frontend): Melhora responsividade da DashboardPage

--- conectainclusao-frontend/src/pages/DashboardPage.jsx
+++ conectainclusao-frontend/src/pages/DashboardPage.jsx
@@ -1,15 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import Button from "../components/Button";
 
 function DashboardPage() {
   console.log("DashboardPage: Componente renderizou!");
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-6 bg-gray-100">
-      <h2 className="mb-6 text-4xl font-extrabold text-center text-blue-700">
+    <div className="flex flex-col items-center justify-center min-h-screen p-6 text-center bg-gray-100 sm:p-8">
+      <h2 className="mb-6 text-3xl font-extrabold text-blue-700 sm:text-4xl">
         Bem-vindo ao Conecta Inclusão!
       </h2>
-      <p className="max-w-2xl mb-8 text-xl text-center text-gray-700">
+      <p className="max-w-2xl mx-auto mb-8 text-lg text-gray-700 sm:text-xl">
         O Hub de Inclusão que conecta Pessoas com Deficiência (PCDs) a
         oportunidades, recursos e comunidades.
       </p>
